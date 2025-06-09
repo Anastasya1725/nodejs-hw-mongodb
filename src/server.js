c49c67e45828f3c1d9013cc2462aaf282a0fe994
Anastasia <anastasiapanasian@gmail.com>
2025-06-09 12:09:15 +0300

    hw2-mongodb

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import pino from 'pino-http'
+import { getAllContactsController } from './controllers/contacts.js'
+
+export const setupServer = () => {
+  const app = express()
+
+  app.use(cors())
+  app.use(pino())
+    app.use(express.json())
+
+  app.get('/contacts', getAllContactsController)
+
+  app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' })
+  })
+
+  const PORT = process.env.PORT || 3000
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
+}
